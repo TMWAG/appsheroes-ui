87125f1796b4ba13c07c9a69a846efe2f965f156
Валяев Семён <semyon.V@cpabro.vip>
2025-11-11 23:14:13 +0300

    Feat: Imported normalize.css

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,7 @@ import { createApp as createVueApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import { pinia } from './store'
+import 'normalize.css';
 
 export function createApp() {
   const app = createVueApp(App)
