330dcc5ae669c3d6652a9672fc743cd0259dfebb
Валяев Семён <semyon.V@cpabro.vip>
2025-11-12 13:30:29 +0300

    Feat: Imported styles

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { router } from './router'
 import { pinia } from './store'
 import 'normalize.css';
+import '@/shared/styles/index.scss';
 
 export function createApp() {
   const app = createVueApp(App)
