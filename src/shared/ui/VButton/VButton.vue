6af3ae3c02345408dfbd90890c63ea34f8d7b061
Валяев Семён <semyon.V@cpabro.vip>
2025-11-12 15:14:23 +0300

    Feat: Added button component

---
+++ src/shared/ui/VButton/VButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { type ButtonProps } from './VButton';
+import $s from './VButton.module.scss';
+
+const props = withDefaults(
+  defineProps<ButtonProps>(),
+  { variant: 'primary' },
+);
+
+const getVariantClass = (): string | undefined => $s[`btn_${props.variant}`];
+</script>
+
+<template>
+<button :class="[$s.btn, getVariantClass()]">
+  <slot name="icon"></slot>
+  <slot>
+    Button
+  </slot>
+</button>
+</template>
