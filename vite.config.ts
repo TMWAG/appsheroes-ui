42f645c4c22f20a24334e2ad8466e5244bafc805
Валяев Семён <semyon.V@cpabro.vip>
2025-11-19 01:28:13 +0300

    Feat:Installed and configured storybook

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const isStorybookProcess = process.env.npm_lifecycle_event === 'storybook'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
+    !isStorybookProcess && vueDevTools(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
