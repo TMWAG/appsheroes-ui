0f0eba988aa780b0b96e72b97d60ed1c55acc875
Валяев Семён <semyon.V@cpabro.vip>
2025-11-19 04:03:07 +0300

    Feat: Added support for href prop, conditional tag display, defined slots, added VIcon component support

--- src/shared/ui/VButton/VButton.vue
+++ src/shared/ui/VButton/VButton.vue
@@ -1,20 +1,49 @@
 <script setup lang="ts">
-import { type VButtonProps } from './VButton';
+import type { RouteLocationAsStringTyped } from 'vue-router';
 import $s from './VButton.module.scss';
+import { VIcon, type IconNames } from '@/shared/ui/VIcon'
+
+type ButtonVariants = 'primary' | 'secondary' | 'tertiary' | 'negative';
 
 const props = withDefaults(
-  defineProps<VButtonProps>(),
-  { variant: 'primary' },
-);
+  defineProps<{
+    /** Defines variant */
+    variant?: ButtonVariants,
+    /** Defines interactivity  */
+    disabled?: boolean,
+    /** Icon in the middle */
+    icon?: IconNames,
+    /** Icon on the left */
+    iconLeft?: IconNames,
+    /** Icon on the right */
+    iconRight?: IconNames,
+    /** Optional link to router location */
+    href?: RouteLocationAsStringTyped,
+  }>(), {
+  variant: 'primary',
+  disabled: false,
+});
 
-const getVariantClass = (): string | undefined => $s[`btn_${props.variant}`];
+defineSlots<{
+  /** Buttons's inner text */
+  default?: string,
+}>()
+
+const tag = props.href ?
+  props.disabled ? 'span' : 'RouterLink'
+  : 'button';
+
+const getVariantClass = (): string | undefined =>
+  $s[`btn_${props.variant}${ tag==='span' ? '-disabled' : ''}`];
 </script>
 
 <template>
-<button :class="[$s.btn, getVariantClass()]" :disabled="disabled">
-  <slot name="icon"></slot>
-  <slot>
-    Button
-  </slot>
-</button>
+  <component :is="tag" :to="href" :class="[$s.btn, getVariantClass()]" :disabled="disabled">
+    <VIcon v-if="iconLeft" :name="iconLeft" :class-name="$s.btn__icon"/>
+    <VIcon v-if="icon" :name="icon" :class-name="$s.btn__icon"/>
+    <span v-else>
+      <slot/>
+    </span>
+    <VIcon v-if="iconRight" :name="iconRight" :class-name="$s.btn__icon"/>
+  </component>
 </template>
