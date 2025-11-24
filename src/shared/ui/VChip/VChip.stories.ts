5d886b576a74d78091a99944af9e7e577fc1d602
Валяев Семён <semyon.V@cpabro.vip>
2025-11-24 16:00:36 +0300

    Feat: New Chip component

---
+++ src/shared/ui/VChip/VChip.stories.ts
@@ -0,0 +1,90 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
+import VChip from "./VChip.vue";
+
+const meta = {
+  component: VChip,
+  title: 'Chip',
+  tags: ['autodocs'],
+  render: (args) => ({
+    components: { VChip },
+    setup() {
+      return { args };
+    },
+    template: '<VChip v-bind="args">{{args.default}}</VChip>',
+  })
+
+} satisfies Meta<typeof VChip>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    default: 'Chip',
+  },
+};
+
+export const Accent: Story = {
+  args: {
+    variant: 'accent',
+    default: 'Chip',
+  },
+};
+
+export const Negative: Story = {
+  args: {
+    variant: 'negative',
+    default: 'Chip',
+  },
+};
+
+export const Info: Story = {
+  args: {
+    variant: 'info',
+    default: 'Chip',
+  },
+};
+
+export const Positive: Story = {
+  args: {
+    variant: 'positive',
+    default: 'Chip',
+  },
+};
+
+export const RemovableDefault: Story = {
+  args: {
+    ...Default.args,
+    removable: true,
+  },
+};
+
+export const RemovableAccent: Story = {
+  args: {
+    ...Accent.args,
+    removable: true,
+  },
+};
+
+export const RemovableNegative: Story = {
+  args: {
+    ...Negative.args,
+    removable: true,
+  },
+};
+
+export const RemovableInfo: Story = {
+  args: {
+    ...Info.args,
+    removable: true,
+  },
+};
+
+export const RemovablePositive: Story = {
+  args: {
+    ...Positive.args,
+    removable: true,
+  },
+};
---
+++ src/shared/ui/VChip/VChip.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { VIcon, type IconNames } from '../VIcon';
+import $s from './VChip.module.scss';
+
+type ChipVariants = 'default' | 'accent' | 'negative' | 'info' | 'positive';
+
+const props = withDefaults(
+  defineProps<{
+    /** Defines variant */
+    variant?: ChipVariants,
+    /** Icon on the left side of main content */
+    iconLeft?: IconNames,
+    /** Icon on the right side of main content */
+    iconRight?: IconNames,
+    /** Defines presents of close button and emission on closed event */
+    removable?: boolean,
+  }>(), {
+  variant: 'default',
+  removable: false,
+})
+
+const emits = defineEmits<{
+  /** Triggers when close button clicked */
+  removed: [],
+  /** Trigger when chip body being clicked */
+  clicked: [],
+}>();
+
+defineSlots<{
+  /** Chip's inner text */
+  default?: string,
+}>();
+
+function getVariantClass(): string | undefined {
+  return $s[`chip_${props.variant}`]
+}
+</script>
+
+<template>
+  <div :class="[$s.chip, getVariantClass()]" @click.self="emits('clicked')">
+    <VIcon v-if="iconLeft" :name="iconLeft" :class-name="$s.chip__icon" />
+    <slot>
+      Chip
+    </slot>
+    <VIcon v-if="iconRight" :name="iconRight" :class-name="$s.chip__icon" />
+    <button v-if="removable" aria-label="remove" type="button" :class="$s.chip__remove" @click="emits('removed')">
+      <VIcon name="x" :class-name="$s.chip__remove_icon" aria-label="remove" />
+    </button>
+  </div>
+</template>
