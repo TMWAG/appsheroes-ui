90cf960847c622ac57f5eb1f5b565434acb5e6e1
Валяев Семён <semyon.V@cpabro.vip>
2025-11-27 10:45:32 +0300

    feat(components): new component input

---
+++ src/shared/ui/VInput/VInput.stories.ts
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
+import VInput from "./VInput.vue";
+
+const meta = {
+  component: VInput,
+  title: 'Input',
+  render: (args) => ({
+    components: { VInput },
+    setup() {
+      return { args };
+    },
+    template: '<VInput v-bind="args"/>',
+  })
+} satisfies Meta<typeof VInput>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: 'input',
+    name: 'test',
+  },
+};
+
+export const Resettable: Story = {
+  args: {
+    ...Default.args,
+    resettable: true,
+  }
+}
+
+export const Filled: Story = {
+  args: {
+    label: 'Label',
+    name: 'test',
+    value: 'Value'
+  }
+};
+
+export const Caption: Story = {
+  args: {
+    label: 'Label',
+    name: 'test',
+    caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae nesciunt maiores aliquid, hic earum dicta, iste laborum doloribus sed praesentium inventore ipsa omnis voluptate sapiente natus recusandae pariatur libero.',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    label: 'Label',
+    name: 'test',
+    error: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae nesciunt maiores aliquid, hic earum dicta, iste laborum doloribus sed praesentium inventore ipsa omnis voluptate sapiente natus recusandae pariatur libero.'
+  },
+};
+
+export const ErrorAndCaption: Story = {
+  args: {
+    label: 'Label',
+    name: 'test',
+    caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae nesciunt maiores aliquid, hic earum dicta, iste laborum doloribus sed praesentium inventore ipsa omnis voluptate sapiente natus recusandae pariatur libero.',
+    error: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quae nesciunt maiores aliquid, hic earum dicta, iste laborum doloribus sed praesentium inventore ipsa omnis voluptate sapiente natus recusandae pariatur libero.'
+  }
+}
---
+++ src/shared/ui/VInput/VInput.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import $s from './VInput.module.scss';
+import { onMounted, onUpdated, ref, type Ref } from 'vue';
+import VTooltip from '../VTooltip/VTooltip.vue';
+import VIcon from '../VIcon/VIcon.vue';
+
+const props = withDefaults(
+  defineProps<{
+    /** Defines input's label */
+    label: string;
+    /** Optionally defines input's name, if needed in form data constructor */
+    name?: string;
+    /** Input's value */
+    value?: string;
+    /** Optional input's caption, use for notices */
+    caption?: string;
+    /** Error message on failed validation */
+    error?: string;
+    /** Defines presence of reset button */
+    resettable?: boolean
+  }>(), { name: 'input', resettable: false }
+);
+
+const emits = defineEmits<{
+  input: [value: string],
+  reset: [],
+}>();
+
+function generateId(name: string): string {
+  const number = Math.floor(Math.random() * 1000)
+  return `${name}-${number}}`;
+};
+
+const inputId = generateId(props.name);
+const errorId = generateId('error');
+const captionId = generateId('caption');
+
+const input = ref<HTMLElement>();
+const captionBlock = ref<HTMLSpanElement>();
+const errorBlock = ref<HTMLSpanElement>();
+
+const isCaptionWiderThanBlock = ref<boolean>(false);
+const isErrorWiderThanBlock = ref<boolean>(false);
+
+/**
+ * Used to determine if text content of block will fit in one line and not be wider than parent element
+ * @param text String which width should be measured
+ * @param block Ref to a block containing text
+ * @param parent Ref to a parent containing block
+ * @throws when values are not provided
+ */
+function isBlockWiderThanParent(
+  text: string | undefined,
+  block: Ref<Element | undefined>,
+  parent: Ref<Element | undefined>
+): boolean {
+  if (!parent.value)
+    throw Error('No parent element was provided');
+
+  if (!text || !block.value)
+    throw Error('No text or block was provided');
+
+  const { fontStyle, fontWeight, fontSize, fontFamily } = getComputedStyle(block.value);
+  const fontString = `${fontStyle} ${fontWeight} ${fontSize} ${fontFamily}`;
+  const canvas = document.createElement('canvas');
+  let context = canvas.getContext('2d');
+  if (!context)
+    throw Error('No canvas context was provided');
+  context.font = fontString;
+  const width = context.measureText(text).width;
+  canvas.remove();
+  context = null;
+  return width > parent.value.clientWidth;
+};
+
+function determineWidthOfBlocks() {
+  try {
+    if (props.caption)
+      isCaptionWiderThanBlock.value = isBlockWiderThanParent(props.caption, captionBlock, input);
+    if (props.error)
+      isErrorWiderThanBlock.value = isBlockWiderThanParent(props.error, errorBlock, input);
+  } catch (err) {
+    if (err instanceof Error)
+      console.error(err.message);
+  }
+};
+
+onMounted(() => {
+  determineWidthOfBlocks();
+});
+
+onUpdated(() => {
+  determineWidthOfBlocks();
+});
+</script>
+
+<template>
+  <div :class="$s.input" ref="input">
+    <div :class="$s.input__wrapper">
+      <button v-if="resettable && value" :class="$s.input__reset_button" @click.prevent="emits('reset')">
+        <VIcon name="x" :class-name="$s.input__reset_icon"/>
+      </button>
+      <input type="text" placeholder=" " :name="name" :id="inputId" :value="value" :class="$s.input__input"
+        @input="emits('input', ($event.target as HTMLInputElement).value)">
+      <label :class="$s.input__label" :for="inputId">
+        {{ label }}
+      </label>
+    </div>
+    <div :class="$s.input__footer">
+      <VTooltip v-if="isCaptionWiderThanBlock && caption" :text="caption" :id="captionId">
+        <span v-show="caption" :class="$s.input__caption">{{ caption }}</span>
+      </VTooltip>
+      <span v-if="caption && !isCaptionWiderThanBlock" ref="captionBlock" :class="$s.input__caption">
+        {{ caption }}
+      </span>
+      <VTooltip v-if="isErrorWiderThanBlock && error" :text="error" :class-name="$s.input__error_tooltip" :id="errorId">
+        <span :class="$s.input__error">{{ error }}</span>
+      </VTooltip>
+      <span v-if="error && !isErrorWiderThanBlock" ref="errorBlock" :class="$s.input__error">
+        {{ error }}
+      </span>
+    </div>
+  </div>
+</template>
